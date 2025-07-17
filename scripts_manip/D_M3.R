83f6eacbfc487741587682d015bc6725ed58f68c
Alice Thomassin <alice.thomassin@gmail.com>
2025-07-17 18:36:06 +0200

    retrait cols empty et rename

---
+++ scripts_manip/D_M3.R
@@ -0,0 +1,49 @@
+# PROFESSION ####
+M3_F0 <- read_sas("../raw_data/gb_ddb_id_01_pr_03.sas7bdat")
+
+## Remove empty cols ####
+M3_F1 <- M3_F0 %>% 
+  mutate(across(
+    .cols = where(is.character),
+    .fns = ~ na_if(., "")
+  )) %>% 
+  remove_empty("cols")
+
+## Rename cols ####
+
+cols_sap <- M3_F1 %>% 
+  select(all_of(starts_with("sap_")),
+         statut_emploi,
+         parc_prof) %>% 
+  colnames()
+
+cols_pp <- M3_F1 %>% 
+  select(all_of(starts_with("pp_"))) %>% 
+  colnames()
+
+M3_F2 <- M3_F1 %>% 
+  rename_with(~ paste0("pr_", .x), all_of(cols_sap)) %>% 
+  rename_with(~ gsub("^pp_", "pr_", .x), all_of(cols_pp)) %>% 
+  rename("id_tab_db" = "tab_db",
+         "pr_sap_csp_prof" = "csp_sap_prof",
+         "pr_commentaires" = "commentaires_prof") %>% 
+  mutate(across(
+    .cols = where(is.character),
+    .fns = ~ na_if(., "")
+  )) %>% 
+  separate(id_sep,
+           into = c("rid", "id_link"),
+           sep = "_",
+           remove = FALSE,
+           fill = "right") %>%
+  select(-rid) %>% 
+  mutate(id_link = case_when(
+    is.na(id_link) ~ id_anonymat,
+    TRUE ~ id_link
+  ))
+
+### Clean up
+rm(list = c("M3_F0",
+            "M3_F1",
+            "cols_pp",
+            "cols_sap"))
