4aa987f07f9507ff5d3cece4fe97245e27d3c0a5
Alice Thomassin <alice.thomassin@gmail.com>
2025-07-17 20:31:55 +0200

    M3_int_F2: Remove empty cols & rename

--- scripts_manip/D_M3.R
+++ scripts_manip/D_M3.R
@@ -111,11 +111,76 @@ M1_V3 <- left_join(
 rm(list = c("common_vars",
             "verif_join"))
 
+# INTERRUPTIONS PRO ####
+
+## Remove empty cols ####
+M3_int_F0 <- read_sas("../raw_data/gb_ddb_pr_03_int_03.sas7bdat")
+
+M3_int_F1 <- M3_int_F0 %>% 
+  mutate(across(
+    .cols = where(is.character),
+    .fns = ~ na_if(., "")
+  )) %>% 
+  remove_empty("cols")
+
+## Rename cols ####
+
+# Colonnes où on ajoute le préfix pr
+cols_add_pr <- M3_int_F1 %>% 
+  select(all_of(starts_with("sap_")),
+         statut_emploi,
+         parc_prof) %>% 
+  colnames()
+
+# Colonnes où on enlève le préfix pcq concerne events multiples
+cols_rmv_pp_interromp <- M3_int_F1 %>% 
+  select(all_of(starts_with("pp_interromp_"))) %>% 
+  colnames()
+
+# Colonnes où on enlève aussi le préfixe pcq concerne events multiples
+# mais la structure des variables est différente
+cols_rmv_pp <- M3_int_F1 %>% 
+  select(all_of(starts_with("pp_reprise_")),
+         pp_duree_j,
+         pp_non_repr) %>% 
+  colnames()
+
+# Variables où ne fait que changer le préfix
+cols_sub_pr <- M3_int_F1 %>% 
+  select(all_of(starts_with("pp_")), -all_of(c(cols_rmv_pp_interromp, 
+                                               cols_rmv_pp))) %>% 
+  colnames()
 
 
+M3_int_F2 <- M3_int_F1 %>% 
+  rename_with(~ paste0("pr_", .x), all_of(cols_add_pr)) %>%
+  rename_with(~ gsub("^pp_interromp_", "", .x), all_of(cols_rmv_pp_interromp)) %>%
+  rename_with(~ gsub("^pp_", "", .x), all_of(cols_rmv_pp)) %>% 
+  rename_with(~ gsub("^pp_", "pr_", .x), all_of(cols_sub_pr)) %>%
+  rename("id_tab_db" = "tab_db",
+         "pr_commentaires" = "commentaires_prof",
+         "pr_int_date_creation" = "int_date_creation",
+         "pr_int_interromp"  = "int_pp_interromp",
+         "pr_int_type" = "int_type") %>% 
+  mutate(across(
+    .cols = where(is.character),
+    .fns = ~ na_if(., "")
+  ))%>% 
+  separate(id_sep,
+           into = c("rid", "id_link"),
+           sep = "_",
+           remove = FALSE,
+           fill = "right") %>%
+  select(-rid) %>% 
+  mutate(id_link = case_when(
+    is.na(id_link) ~ id_anonymat,
+    TRUE ~ id_link
+  ))
 
-
-
-
+### Clean up
+rm(list = c("cols_add_pr",
+            "cols_rmv_pp_interromp",
+            "cols_rmv_pp",
+            "cols_sub_pr"))
 
 
