eee39a4d2054bb02dae20e6831097551c8799387
Alice Thomassin <alice.thomassin@gmail.com>
2025-07-18 17:07:55 +0200

    Ma_cpl_F2 : rename, remove empty cols

---
+++ scripts_manip/G_M4_cpl.R
@@ -0,0 +1,101 @@
+# 3.2 COUPLES ####
+M4_cpl_F0 <- read_sas("../raw_data/gb_ddb_fa_04_couple_04.sas7bdat")
+
+## 3.2.1 Remove empty cols ####
+M4_cpl_F1 <- M4_cpl_F0 %>% 
+  mutate(across(
+    .cols = where(is.character),
+    .fns = ~ na_if(., "")
+  )) %>% 
+  remove_empty("cols") %>%
+  rowwise() %>% 
+  mutate(
+    sf_conjt_an_nais = case_when(
+      is.na(sf_conjt_an_nais) & !is.na(sf_an_conjt_nais) ~ sf_an_conjt_nais,
+      TRUE ~ sf_conjt_an_nais
+  )) %>% 
+  ungroup() %>% 
+  select(-sf_an_conjt_nais,
+         -couple_sf_couple_1an) # Retirer vars inutiles
+
+## 3.2.2 Rename cols ####
+vars_add_fa <- M4_cpl_F1 %>% 
+  select(all_of(starts_with("fo_")),
+         couple_date_creation,
+         couple_type) %>% 
+  colnames()
+
+vars_sub_pf <- M4_cpl_F1 %>% 
+  select(all_of(starts_with("pf_mere_")),
+         all_of(starts_with("pf_pere_"))) %>% 
+  colnames()
+
+vars_sub_sf <- M4_cpl_F1 %>% 
+  select(all_of(starts_with("sf_")),
+         -all_of(contains("_conjt")),
+         -all_of(contains("interromp")),
+         -sf_nature_union,
+         -sf_raison_muco) %>% 
+  colnames()
+
+vars_conjt <- M4_cpl_F1 %>% 
+  select(sf_conjt_enf,
+         sf_conjt_muco,
+         sf_conjt_prenom,
+         sf_conjt_sexe) %>% 
+  colnames()
+
+M4_cpl_F2 <- M4_cpl_F1 %>% 
+  rename_with(~ paste0("fa_", .x), all_of(vars_add_fa)) %>%
+  rename_with(~ gsub("^pf_", "fa_", .x), all_of(vars_sub_pf)) %>%
+  rename_with(~ gsub("^sf_", "fa_", .x), all_of(vars_sub_sf)) %>%
+  rename_with(~ gsub("^sf_", "", .x), all_of(vars_conjt)) %>%
+  rename("id_tab_db" = "tab_db",
+         "fa_commentaires" = "commentaires_fam",
+         "fa_pm_union_an" = "pf_union_an",
+         "fa_pm_union" = "pf_union_parents",
+         "fa_pm_union_mois" = "pf_union_mois",
+         "fa_rang_nais" = "pf_rang_nais",
+         "fa_pm_union_int_muco" = "pf_raison_muco",
+         "fa_pm_union_int" = "pf_parents_interromp",
+         "fa_pm_union_int_an" = "pf_interromp_an",
+         "fa_pm_union_int_mois" = "pf_interromp_mois",
+         "fa_pm_union_int_par" = "pf_interromp_par",
+         "fa_fratrie" = "pf_fratrie",
+         "int_an" = "sf_an_interromp",
+         "int" = "sf_interromp",
+         "int_par" = "sf_interromp_par",
+         "int_mois" = "sf_mois_interromp",
+         "union_nature" = "sf_nature_union",
+         "int_muco" = "sf_raison_muco",
+         "conjt_union_an" = "sf_conjt_an_union",
+         "conjt_union_mois" = "sf_conjt_mois_union",
+         "conjt_nais_an" = "sf_conjt_an_nais",
+         "conjt_nais_mois" = "sf_conjt_mois_nais") %>% 
+  mutate(across(
+    .cols = where(is.character),
+    .fns = ~ na_if(., "")
+  )) %>% 
+  separate(id_sep,
+           into = c("rid", "id_link"),
+           sep = "_",
+           remove = FALSE,
+           fill = "right") %>%
+  select(-rid) %>% 
+  mutate(id_link = case_when(
+    is.na(id_link) ~ id_anonymat,
+    TRUE ~ id_link
+  )) %>% 
+  mutate(id_date_nais = case_when(
+    id_anonymat %in% id_DDN ~ DDN,
+    TRUE ~ id_date_nais
+  ))
+
+### Clean up
+rm(list = c("M4_cpl_F0",
+            "M4_cpl_F1",
+            "vars_add_fa",
+            "vars_sub_pf",
+            "vars_sub_sf",
+            "vars_conjt"))
+
