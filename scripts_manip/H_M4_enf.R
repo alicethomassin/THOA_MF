00c194b3efadbd42adf76e684d2b911bd4738c8d
Alice Thomassin <alice.thomassin@gmail.com>
2025-07-19 15:28:53 +0200

    M4_enf_F2: rename cols

--- scripts_manip/H_M4_enf.R
+++ scripts_manip/H_M4_enf.R
@@ -1,2 +1,94 @@
 # 3.3 ENFANTS ####
 M4_enf_F0 <- read_sas("../raw_data/gb_ddb_fa_04_enfants_04.sas7bdat")
+
+## 3.3.1 Remove empty cols ####
+M4_enf_F1 <- M4_enf_F0 %>% 
+  mutate(across(
+    .cols = where(is.character),
+    .fns = ~ na_if(., "")
+  )) %>% 
+  remove_empty("cols") %>% 
+  select(-enfants_sf_enfants)
+
+## 3.3.2 Rename cols ####
+vars_add_fa <- M4_enf_F1 %>% 
+  select(all_of(starts_with("fo_")),
+         enfants_date_creation,
+         enfants_type) %>% 
+  colnames()
+
+vars_sub_pf <- M4_enf_F1 %>% 
+  select(all_of(starts_with("pf_mere_")),
+         all_of(starts_with("pf_pere_")),
+         pf_fratrie) %>% 
+  colnames()
+
+vars_sub_sf <- M4_enf_F1 %>% 
+  select(all_of(starts_with("sf_")),
+         -all_of(contains("_enf_")),
+         all_of(contains("_nb"))) %>% 
+  colnames()
+
+vars_enfants <- M4_enf_F1 %>% 
+  select(all_of(starts_with("sf_enf")),
+         -sf_enf_adopte_nb,
+         -sf_enf_bio_nb) %>% 
+  colnames()
+
+
+M4_enf_F2 <- M4_enf_F1 %>% 
+  rename_with(~ paste0("fa_", .x), all_of(vars_add_fa)) %>%
+  rename_with(~ gsub("^pf_", "fa_", .x), all_of(vars_sub_pf)) %>%
+  rename("id_tab_db" = "tab_db",
+         "fa_commentaires" = "commentaires_fam",
+         "fa_pm_union_an" = "pf_union_an",
+         "fa_pm_union" = "pf_union_parents",
+         "fa_pm_union_mois" = "pf_union_mois",
+         "fa_rang_nais" = "pf_rang_nais",
+         "fa_pm_union_int_muco" = "pf_raison_muco",
+         "fa_pm_union_int" = "pf_parents_interromp",
+         "fa_pm_union_int_an" = "pf_interromp_an",
+         "fa_pm_union_int_mois" = "pf_interromp_mois",
+         "fa_pm_union_int_par" = "pf_interromp_par",
+         "adopte_an" = "sf_enf_an_adopte",
+         "adopte_mois" = "sf_enf_mois_adopte",
+         "nais_an" = "sf_enf_an_nais",
+         "nais_mois" = "sf_enf_mois_nais",
+         "deces_an" = "sf_enf_an_deces",
+         "deces_mois" = "sf_enf_mois_deces") %>% 
+  rename_with(~ gsub("^sf_enf_", "", .x), any_of(vars_enfants)) %>%
+  rename_with(~ gsub("^sf_", "fa_", .x), all_of(vars_sub_sf)) %>% 
+  mutate(across(
+    .cols = where(is.character),
+    .fns = ~ na_if(., "")
+  )) %>% 
+  separate(id_sep,
+           into = c("rid", "id_link"),
+           sep = "_",
+           remove = FALSE,
+           fill = "right") %>%
+  select(-rid) %>% 
+  mutate(id_link = case_when(
+    is.na(id_link) ~ id_anonymat,
+    TRUE ~ id_link
+  )) %>% 
+  mutate(id_date_nais = case_when(
+    id_anonymat %in% id_DDN ~ DDN,
+    TRUE ~ id_date_nais
+  ))
+  
+### Clean up 
+rm(list = c("vars_add_fa",
+            "vars_sub_pf",
+            "vars_sub_sf",
+            "vars_enfants",
+            "M4_enf_F0",
+            "M4_enf_F1"))
+  
+  
+  
+  
+  
+  
+  
+  
