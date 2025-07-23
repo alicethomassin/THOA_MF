321599ff0c4a1e69b9c06796bca0fdf609b516f2
Alice Thomassin <alice.thomassin@gmail.com>
2025-07-23 11:46:34 +0200

    Mise à jour trame

--- scripts_manip/01_trame.R
+++ scripts_manip/01_trame.R
@@ -2,3 +2,19 @@
 source("scripts_manip/A_M2.R")
 
 source("scripts_manip/B_M2_rdb.R")
+
+source("scripts_manip/C_M2_int.R")
+
+source("scripts_manip/D_M3.R")
+
+source("scripts_manip/E_M3_int.R")
+
+source("scripts_manip/F_M4.R")
+
+source("scripts_manip/G_M4_cpl.R")
+
+source("scripts_manip/H_M4_enf.R")
+
+source("scripts_manip/J_M5_log.R")
+
+source("scripts_manip/K_M6_qdv.R")
