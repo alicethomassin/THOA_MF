d9deddc23e4ed1a3d1be55479a28b45b7a2c0924
Alice Thomassin <alice.thomassin@gmail.com>
2025-07-30 14:10:50 +0200

    Modifier nom variables

--- nettoyage/A_scolarite.R
+++ nettoyage/A_scolarite.R
@@ -3,11 +3,10 @@ library(tidyverse)
 
 # Pour importer les données SAS
 library(haven)
+library(questionr)
 
-# Pour identifier les colonnes vides
-library(janitor)
-
-# SCOLARITÉ ####
+# 1. SCOLARITÉ ####
+## 1.1 rename ####
 
 M2_F0 <- read_sas("../raw_data/gb_ddb_id_01_sc_02.sas7bdat")
 
@@ -35,7 +34,7 @@ M2_F1 <- M2_F0 %>%
          "sc_debut_age" = "sc_age_debut",
          "sc_debut_date" = "sc_date_debut")
 
-## Remove duplicates ####
+## 1.2 remove duplicates ####
 
 doublons <- M2_F1 %>% 
   filter(is.na(id_date_creation)) %>% 
@@ -84,4 +83,74 @@ M2_F2 <- bind_rows(
   mutate(id_link = case_when(
     is.na(id_link) ~ id_anonymat,
     TRUE ~ id_link
+  )) %>% 
+  mutate(
+    across(
+      .cols = where(is.character),
+      .fns = ~ na_if(., "") # Changer les colonnes en NA
+    )
+  )
+
+# CLean up
+rm(list = c("clean",
+            "doublons",
+            "identity",
+            "cols_fa",
+            "cols_ps",
+            "ids_TPO"))
+
+# 2. REDOUBLEMENT ####
+M2_rdb_F0 <- read_sas("../raw_data/gb_ddb_sc_02_rdb_02.sas7bdat")
+## 2.1 rename ####
+
+cols_fa <- M2_rdb_F0 %>% 
+  select(all_of(starts_with("fa_")),
+         -fa_an_diplome) %>% 
+  colnames()
+
+cols_ps <- M2_rdb_F0 %>% 
+  select(all_of(starts_with("ps_")),
+         -ps_an_debut) %>% 
+  colnames()
+
+cols_rdb <- M2_rdb_F0 %>% 
+  select(all_of(starts_with("rdb_")), -rdb_ps_redoubl) %>% 
+  colnames()
+
+cols_rd <- M2_rdb_F0 %>% 
+  select(all_of(starts_with("rd_"))) %>% 
+  colnames()
+
+M2_rdb_F1 <- M2_rdb_F0 %>%  
+  rename_with(~ gsub("^fa_", "sc_", .x), all_of(cols_fa)) %>% 
+  rename_with(~ gsub("^ps_", "sc_", .x), all_of(cols_ps)) %>%
+  rename_with(~ gsub("^rdb_", "sc_rdb_", .x), all_of(cols_rdb)) %>%
+  rename_with(~ gsub("^rd_", "", .x), all_of(cols_rd)) %>%
+  rename("id_tab_db" = "tab_db",
+         "sc_rdb_redoubl" = "rdb_ps_redoubl",
+         "sc_diplome_an" = "fa_an_diplome",
+         "sc_id_cat" = "id_sc_cat",
+         "ps_debut_an" = "ps_an_debut",
+         "sc_debut_age" = "sc_age_debut",
+         "sc_debut_date" = "sc_date_debut") %>% 
+  mutate(across(
+    .cols = where(is.character),
+    .fns = ~ na_if(., "")
+  )) %>% 
+  separate(id_sep,
+           into = c("rid", "id_link"),
+           sep = "_",
+           remove = FALSE,
+           fill = "right") %>%
+  select(-rid) %>% 
+  mutate(id_link = case_when(
+    is.na(id_link) ~ id_anonymat,
+    TRUE ~ id_link
   ))
+
+### Clean up 
+rm(list = c("M2_rdb_F0",
+            "cols_fa",
+            "cols_ps",
+            "cols_rd",
+            "cols_rdb"))
