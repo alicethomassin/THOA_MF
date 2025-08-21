f733e9f266625b74a68fba38735f22580eb1abf9
Alice Thomassin <alice.thomassin@gmail.com>
2025-08-21 17:48:40 +0200

    fonctions recoder variables

--- nettoyage/A_scolarite.R
+++ nettoyage/A_scolarite.R
@@ -390,6 +390,133 @@ rm(list = c("M2_rdb_F0",
             "cols_rd",
             "cols_rdb"))
 
+## 2.2 Retirer doublons ####
+# Ne garder que les colonnes sur le redoublement et les quelques variables
+# pour permettre la liaison avec la base générale sur la scolarité.
+# Je retire aussi id_age_cat pcq elle correspond déjà à id_age_cat_2
+rdb_F0 <- M2_rdb_F1 %>% 
+  select(id_anonymat, id_link, id_date_creation, setdiff(names(.), names(M2_F2)), -id_age_cat) %>% 
+  rename("sc_redoubl" = "sc_rdb_redoubl") # Je renomme aussi cette variable 
+                                          # avec le même nom que dans M2_F2
+# Je pourrais renommer cette variable plus tôt, je ne sais pas encore ce que 
+# va donner ce changement. 
+
+doublons <- rdb_F0 %>% 
+  filter(is.na(id_date_creation))
+
+identity_main <- M1_identity %>% 
+  filter(id_link %in% doublons$id_link) %>% 
+  select(id_anonymat, id_link, id_date_creation)
+
+# Ce sont les données du redoublement renseignées lors de la connexion avec
+# l'identifiant du doublon
+doublons_clean <- left_join(
+  doublons %>% 
+    select(-id_anonymat, -id_date_creation),
+  identity_main,
+  by = "id_link",
+  relationship = "many-to-many"
+)
+
+# Ici, ce sont les données du redoublement renseignées lors de la connexion
+# avec l'identifiant de l'identité
+identity_rdb <- rdb_F0 %>% 
+  filter(id_link %in% doublons$id_link &
+           !is.na(id_date_creation))
+
+# Regrouper ces deux bases de rensignements sur le redoublement pour repérer
+# les doubles renseignements d'un même redoublement
+together <- bind_rows(
+  doublons_clean %>% 
+    mutate(source = "db"),
+  identity_rdb %>% 
+    mutate(source = "id")
+) %>%  
+  group_by(id_anonymat, an) %>% 
+  mutate(n = n()) %>%
+  arrange(n, id_anonymat, an) %>%
+  relocate(id_anonymat, n, an, classe, source, id_date_creation, sc_rdb_date_creation) %>% 
+  ungroup()
+
+# Retirer à la main, les événements doubles et modifier les modalités de certaines
+# variables qui ont été générées automatiquement
+together_c <- together %>% 
+   slice(c(2,
+           3,
+           5,
+           6,7,
+           8,
+           10,
+           12,
+           14,
+           16,
+           18)) %>% 
+  select(-n, -source) %>% 
+  mutate(sc_redoubl = 1,
+         sc_rdb_cat = "sc_02_rdb_02"
+         )
+# Liste des identifiants à retirer avant de joindre la base corrigée
+ids_TPO <- union(unique(together_c$id_link), unique(together_c$id_anonymat))
+
+# Joindre les deux bases
+rdb_F1 <- bind_rows(
+  rdb_F0 %>% 
+    filter(!id_anonymat %in% ids_TPO,
+           !is.na(sc_rdb_date_creation)),
+  together_c
+)
+
+## 2.3 Corriger valeurs manquantes ####
+
+rcd_year <- function(df, borne, var_year){
+  df %>% 
+    mutate(
+      {{var_year}} := as.character(case_when(
+        {{borne}} == 1 & is.na({{var_year}}) ~ "5555", # Concerné, mais pas répondu
+        {{borne}} == 0 | {{borne}} > 1 ~ "7777",       # Pas concerné
+        is.na({{borne}}) ~ NA_character_,               # Pas participé
+        TRUE ~ as.character({{var_year}})                           # Réponse renseignée
+      )
+    ))
+}
+
+rcd_vars <- function(df, borne, vars){
+  df %>%
+    mutate(
+      across(
+        {{vars}},
+        ~ as.character(case_when(
+        {{borne}} == 1 & is.na(.x) ~ "555", # Concerné, mais pas répondu
+        {{borne}} == 0 | {{borne}} > 1 ~ "777",       # Pas concerné
+        is.na({{borne}}) ~ NA_character_,               # Pas participé
+        TRUE ~ as.character(.x)                           # Réponse renseignée
+        ))
+        )
+      )
+}
+
+rcd_chr <- function(df, borne, vars){
+  df %>%
+    mutate(
+      across(
+        {{vars}},
+        ~ as.character(case_when(
+          {{borne}} == 1 & is.na(.x) ~ "Non renseigné", # Concerné, mais pas répondu
+          {{borne}} == 0 | {{borne}} > 1 ~ "Non concerné",       # Pas concerné
+          is.na({{borne}}) ~ NA_character_,               # Pas participé
+          TRUE ~ as.character(.x)                           # Réponse renseignée
+        ))
+      )
+    )
+}
+
+rdb_F2 <- rdb_F1 %>% 
+  group_by(id_anonymat) %>% 
+  rcd_year(sc_redoubl, an) %>% 
+  rcd_vars(sc_redoubl, c(cause, classe)) %>%
+  rcd_chr(sc_redoubl, classe_cat) %>% 
+  ungroup()
+
 ## 2.2 Remove duplicates ####
 common_vars <- intersect(names(M2_rdb_F1), names(M2_F2))
 
