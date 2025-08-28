ae2237edc227a8bc4534bdaf195287d581e6d366
Alice Thomassin <alice.thomassin@gmail.com>
2025-08-28 18:52:58 +0200

    RECODAGE DE INTERRUPTIONS, pivot wide pas fonctionne pcq 105 ligne plutot que 104

--- nettoyage/A_scolarite.R
+++ nettoyage/A_scolarite.R
@@ -221,7 +221,7 @@ P2_rdb_F2 <- bind_rows(
   mutate(
     sc_rdb_cor = case_when(
       sc_rdb_redoubl == 1 ~ 1,
-      is.na(sc_rdb_redoubl) ~ 33
+      is.na(sc_rdb_redoubl) ~ 333
   ))
 # Je conserve ceux qui n'ont pas participé, mais qui ont déclaré avoir
 # eu un redoublement pour ajouter la variable sc_rdb_cor et lui attribuer des
@@ -244,7 +244,7 @@ P2_rdb_F3 <- P2_rdb_F2 %>%
   arrange(classe, an) %>% 
   mutate(
     sc_rdb_nb = case_when(
-      sc_rdb_cor == 33 ~ 333,
+      sc_rdb_cor == 333 ~ 333,
       sc_rdb_cor == 1 ~ n()),
     rang_sc_rdb = case_when(
       sc_rdb_nb == 1 ~ "sc_rdb01",
@@ -270,7 +270,7 @@ rcd_yearsLM <- function(df, borne, vars){
       across(
         {{vars}},
         ~ as.integer(case_when(
-          {{borne}} == 33 & is.na(.x) ~ 3333L,                   # Concerné, pas répondu module
+          {{borne}} == 333 & is.na(.x) ~ 3333L,                   # Concerné, pas répondu module
           {{borne}} == 1 & is.na(.x) ~ 5555L,                    # Concerné, mais pas répondu à la question
           ({{borne}} == 0 | {{borne}} > 1) & is.na(.x)  ~ 7777L, # Pas concerné
           TRUE ~ as.integer(.x)                                  # Réponse renseignée
@@ -285,7 +285,7 @@ rcd_varsLM <- function(df, borne, vars){
       across(
         {{vars}},
         ~ as.integer(case_when(
-          {{borne}} == 33 & is.na(.x) ~ 333L,                  # Concerné, pas répondu au module
+          {{borne}} == 333 & is.na(.x) ~ 333L,                  # Concerné, pas répondu au module
           {{borne}} == 1 & is.na(.x) ~ 555L,                   # Concerné, mais pas répondu
           ({{borne}} == 0 | {{borne}} > 1) & is.na(.x) ~ 777L, # Pas concerné
           TRUE ~ as.integer(.x)                                # Réponse renseignée
@@ -300,7 +300,7 @@ rcd_chrsLM <- function(df, borne, vars){
       across(
         {{vars}},
         ~ as.character(case_when(
-          {{borne}} == 33 & is.na(.x) ~ "Non renseigné (333)",                  # Concerné, pas répondu au module
+          {{borne}} == 333 & is.na(.x) ~ "Non renseigné (333)",                  # Concerné, pas répondu au module
           {{borne}} == 1 & is.na(.x) ~ "Non renseigné (555)",                   # Concerné, mais pas répondu
           ({{borne}} == 0 | {{borne}} > 1) & is.na(.x) ~ "Non concernée (777)", # Pas concerné
           TRUE ~ as.character(.x)                                               # Réponse renseignée
@@ -342,7 +342,7 @@ rcd_yearsWM <- function(df, borne, vars){
       across(
         {{vars}},
         ~ as.integer(case_when(
-          {{borne}} == 33 & is.na(.x) ~ 3333L,    # Concerné, pas répondu module
+          {{borne}} == 333 & is.na(.x) ~ 3333L,    # Concerné, pas répondu module
           {{borne}} == 1 & is.na(.x) ~ 4444L,     # Concerné, mais pas plus de répétitions
           ({{borne}} == 0 | {{borne}} > 1) & is.na(.x)  ~ 7777L, # Pas concerné/Pas répondu à la question borne
           is.na({{borne}}) & is.na(.x) ~ NA_integer_,         # Pas participé
@@ -358,7 +358,7 @@ rcd_varsWM <- function(df, borne, vars){
       across(
         {{vars}},
         ~ as.integer(case_when(
-          {{borne}} == 33 & is.na(.x) ~ 333L,    # Concerné, pas répondu module
+          {{borne}} == 333 & is.na(.x) ~ 333L,    # Concerné, pas répondu module
           {{borne}} == 1 & is.na(.x) ~ 444L,     # Concerné, mais pas répondu
           ({{borne}} == 0 | {{borne}} > 1) & is.na(.x) ~ 777L, # Pas concerné
           is.na({{borne}}) & is.na(.x) ~ NA_integer_,        # Pas participé
@@ -374,7 +374,7 @@ rcd_chrsWM <- function(df, borne, vars){
       across(
         {{vars}},
         ~ as.character(case_when(
-          {{borne}} == 33 & is.na(.x) ~ "Non renseigné (333)",    # Concerné, pas répondu au module
+          {{borne}} == 333 & is.na(.x) ~ "Non renseigné (333)",    # Concerné, pas répondu au module
           {{borne}} == 1 & is.na(.x) ~ "Non concerné (444)",      # Concerné, mais pas de répétition
           ({{borne}} == 0 | {{borne}} > 1) & is.na(.x) ~ "Non concerné (777)",  # Pas concerné
           is.na({{borne}}) & is.na(.x) ~ NA_character_,                     # Pas participé
@@ -398,7 +398,7 @@ M2_F3 <- full_join(
   mutate(
     sc_rdb_cor = case_when(
       sc_redoubl == 0 ~ 0,
-      is.na(sc_rdb_cor) & !is.na(sc_type) ~ 55,
+      is.na(sc_rdb_cor) & !is.na(sc_type) ~ 555,
       is.na(sc_rdb_cor) & is.na(sc_type) ~ NA_integer_,
       TRUE ~ sc_rdb_cor
     )
@@ -430,7 +430,7 @@ M2_F4 <- M2_F3 %>%
   rcd_yearsWM(sc_rdb_cor, all_of(Vars_years)) %>%
   mutate(sc_rdb_nb = case_when(
     sc_rdb_cor == 0 ~ 0,
-    sc_rdb_cor == 55 ~ 555,
+    sc_rdb_cor == 555 ~ 555,
     TRUE ~ sc_rdb_nb
   )) %>%
   rcd_varsWM(sc_rdb_cor, all_of(Vars_rcd)) %>%
@@ -483,6 +483,7 @@ rcd_qcm <- function(df, borne, vars){
       across(
         {{vars}},
         ~ as.integer(case_when(
+          ({{borne}}) == 333 & is.na(.x) ~ 333L,
           ({{borne}} == 0 | {{borne}} > 1) & is.na(.x) ~ 777L, # Pas concerné
           !is.na({{borne}}) & all_na_vars ~ 555L,              # Pas répondu au QCM
           {{borne}} == 1 & is.na(.x) ~ 0L,                     # Pas sélection dans qcm
@@ -542,4 +543,178 @@ M2_F5 <- M2_F4 %>%
   ) %>% 
   relocate(id_anonymat, sc_type, sc_plan, sc_plan_an, sc_plan_demande, sc_fin_etudes, sc_formation, sc_diplome, sc_diplome_an, all_of(S18_ets))
 
+# 4. INTERRUPTIONS SCOLAIRES ####
+M2_int_F0 <- read_sas("../raw_data/gb_ddb_sc_02_int_02.sas7bdat")
+
+## 4.1 Réduire et renommer #### 
+# Réduire aux colonnes qui ne concernent que ce module et créer id_link
+P2_int_F0 <- M2_int_F0 %>% 
+  make_id_link(.) %>% 
+  select(id_anonymat, id_link, id_date_creation,
+         setdiff(names(.), names(M2_F0)), -id_age_cat)
+
+cols_int <- P2_int_F0 %>% 
+  select(all_of(starts_with("int_")), -int_ps_interromp) %>% 
+  colnames()
+
+cols_it <- P2_int_F0 %>% 
+  select(starts_with("it_")) %>% 
+  colnames()
+
+P2_int_F1 <- P2_int_F0 %>% 
+  rename_with(~ gsub("^int_", "sc_int_", .x), all_of(cols_int)) %>%
+  rename_with(~ gsub("^it_", "", .x), all_of(cols_it)) %>%
+  rename("sc_int_interromp" = "int_ps_interromp") %>% 
+  mutate(across(
+    .cols = where(is.character),
+    .fns = ~ na_if(., "")
+  ))
+
+## 4.2 Corriger doublons ####
+doublons <- P2_int_F1 %>% 
+  filter(is.na(id_date_creation))
+
+# Puisque je ne cherche que les id_anonymat, je vais chercher dans la base
+# M1_identity. Si je faisais la recherche locale, il me manquerait des personnes.
+identity_main <- M1_identity %>% 
+  filter(id_link %in% doublons$id_link) %>% 
+  select(id_anonymat, id_link, id_date_creation)
+
+# Ce sont les données du redoublement renseignées lors de la connexion avec
+# l'identifiant du doublon
+doublons_clean <- left_join(
+  identity_main,
+  doublons %>% 
+    select(-id_anonymat, -id_date_creation),
+  by = "id_link",
+  relationship = "many-to-many"
+)
+
+# Ici, ce sont les données du redoublement renseignées lors de la connexion
+# avec l'identifiant de l'identité
+identity_rdb <- P2_int_F1 %>% 
+  filter(id_link %in% doublons$id_link &
+           !is.na(id_date_creation))
+
+# Regrouper ces deux bases de renseignements sur le redoublement pour repérer
+# les doubles renseignements d'un même redoublement scolaire
+together <- bind_rows(
+  doublons_clean %>% 
+    mutate(source = "db"),
+  identity_rdb %>% 
+    mutate(source = "id")
+) %>%  
+  group_by(id_anonymat, an) %>% 
+  mutate(n = n()) %>%
+  arrange(n, id_anonymat, an) %>%
+  relocate(id_anonymat, n, an, classe, source, id_date_creation, sc_int_date_creation) %>% 
+  ungroup()
+
+# Retirer à la main, les événements doubles et modifier les modalités de certaines
+# variables qui ont été générées automatiquement
+together_c <- together %>% 
+  slice(c(1,
+          2,3,
+          4,
+          5,
+          7,9,
+          11,
+          12:19)) %>% 
+  select(-n, -source) %>% 
+  mutate(
+    sc_int_interromp = 1,
+    sc_int_cat = "sc_02_int_02"
+  )
+# Liste des identifiants à retirer avant de joindre la base corrigée
+ids_TPO <- union(unique(together_c$id_link), unique(together_c$id_anonymat))
+
+# Joindre les deux bases
+P2_int_F2 <- bind_rows(
+  P2_int_F1 %>% 
+    filter(!id_anonymat %in% ids_TPO),
+  together_c
+) %>% 
+  mutate(
+    sc_int_cor = case_when(
+      sc_int_interromp == 1 ~ 1,
+      is.na(sc_int_interromp) ~ 333
+    ))
+# Je conserve ceux qui n'ont pas participé, mais qui ont déclaré avoir
+# eu un redoublement pour ajouter la variable sc_int_cor et lui attribuer des
+# nouvelles modalités pour prendre en compte ces situations.
+# Clean up
+rm(list = c("together",
+            "together_c",
+            "ids_TPO",
+            "doublons",
+            "doublons_clean",
+            "identity_main",
+            "identity_rdb",
+            "cols_it",
+            "cols_int"))
+
+## 2.3 Préparer le pivot wider ####
+P2_int_F3 <- P2_int_F2 %>% 
+  group_by(id_anonymat) %>% 
+  arrange(classe, an) %>% 
+  mutate(
+    sc_int_nb = case_when(
+      sc_int_cor == 333 ~ 333,
+      sc_int_cor == 1 ~ n()),
+    rang_sc_int = case_when(
+      sc_int_nb == 1 ~ "sc_int01",
+      TRUE ~ paste0("sc_int", sprintf("%02d", row_number()))
+    )) %>% 
+  relocate(id_anonymat, sc_int_nb, rang_sc_int, an, classe, sc_int_cor) %>% 
+  ungroup() %>% 
+  mutate(across(
+    .cols = where(is.character),
+    .fns = ~ na_if(., "")
+  )) # Besoin de transformer les characters en NA pour l'utilisation des fonctions
+
+# Variables qui seront en plusieurs colonnes
+vars_wider <- P2_int_F3 %>% 
+  select(-starts_with("sc_"),
+         -starts_with("id_"),
+         -rang_sc_int) %>% 
+  colnames()
+
+# Appliquer les fonctions pour recoder les valeurs manquantes avant le pivot wide
+# La raison pour laquelle je le fais d'abord ici, c'est que c'est encore une ligne
+# par événement. Plus tard ce sera un autre code pour la base où c'est une ligne par 
+# personne. Et il faut tout de suite indiquer les situations où les données sont manquantes (555)
+# parce que la personne a sauté cette question, des situations où les données sont
+# manquantes (444) parce que la personne n'a pas eu d'autre évents renouvelables
+vars_qcm <- P2_int_F3 %>% 
+  select(starts_with("cause_"), -cause_autre) %>% 
+  colnames()
+
+P2_int_F4 <- P2_int_F3 %>% 
+  group_by(id_anonymat) %>%
+  rcd_varsLM(sc_int_cor, c(classe, mois)) %>% 
+  rcd_yearsLM(sc_int_cor, an) %>%
+  rcd_qcm(sc_int_cor, all_of(vars_qcm)) %>%
+  rcd_varsLM(sc_int_cor, repr) %>% 
+  rcd_yearsLM(repr, an_repr) %>% 
+  rcd_varsLM(repr, c(classe_repr, mois_repr, interromp_duree)) %>%
+  mutate(
+    non_repr = case_when(
+      repr == 0 & is.na(non_repr) ~ 555L,
+      repr == 333 ~ 333L,
+      repr > 0 ~ 777L,
+      TRUE ~ as.integer(non_repr)
+    )
+  ) %>% 
+  ungroup()
+
+## 2.5 Pivot ####
+# On pivote ensuite en wide en appliquant les nouveaux noms de variables
+P2_int_W1 <- P2_int_F4 %>% 
+  pivot_wider(
+    names_from = rang_sc_int,
+    values_from = all_of(vars_wider),
+    names_glue = "{rang_sc_int}_{.value}"
+  )
+
+###VOIR POURQUOI IL A 105 LIGNES ET NON 104 ####
 
