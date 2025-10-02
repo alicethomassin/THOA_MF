460b898984b4aa2b22df888b33122e25b5b6b153
Alice Thomassin <alice.thomassin@gmail.com>
2025-10-02 15:53:20 +0200

    ajout du renommage

--- scripts_manip/D_M3.R
+++ scripts_manip/D_M3.R
@@ -1,5 +1,39 @@
-# 2.1 PROFESSION ####
+# 2. PROFESSION ####
 M3_F0 <- read_sas("../raw_data/gb_ddb_id_01_pr_03.sas7bdat")
+
+## 2.1 Réduire et renommer #### 
+# Réduire aux colonnes qui ne concernent que ce module et créer id_link
+P3_F0 <- M3_F0 %>% 
+  make_id_link(.) %>% 
+  select(id_anonymat, id_link, id_date_creation,
+         setdiff(names(.), names(M2_F0)))
+
+cols_sap <- P3_F0 %>% 
+  select(all_of(starts_with("sap_")),
+         statut_emploi,
+         parc_prof) %>% 
+  colnames()
+
+cols_pp <- M3_F1 %>% 
+  select(all_of(starts_with("pp_")),
+         -pp_an_prems,
+         -pp_ado_pdt_6mois
+  ) %>% 
+  colnames()
+
+M3_F2 <- M3_F1 %>% 
+  rename_with(~ paste0("pr_", .x), all_of(cols_sap)) %>% 
+  rename_with(~ gsub("^pp_", "pr_", .x), all_of(cols_pp)) %>% 
+  rename("id_tab_db" = "tab_db",
+         "pr_sap_csp_prof" = "csp_sap_prof",
+         "pr_commentaires" = "commentaires_prof",
+         "pr_prems_an" = "pp_an_prems",
+         "pr_pdt_6mois_ado" = "pp_ado_pdt_6mois") %>% 
+  mutate(across(
+    .cols = where(is.character),
+    .fns = ~ na_if(., "")
+  ))
+
 
 ## 2.1.1 Remove empty cols ####
 M3_F1 <- M3_F0 %>% 
